0942dbf0adb18d274e4cf61b6ea72925b094b5b8
Michał Korzeniewski <michal.korzeniewskiii@gmail.com>
2025-04-05 20:05:41 +0200

    added functions to handle outliers

--- notebooks/data.py
+++ notebooks/data.py
@@ -101,10 +101,54 @@ def read_preprocessed_data(file_name="marketing_campaign.csv", std=True, le=True
         LE = LabelEncoder()
         for i in object_cols:
             df[i] = df[[i]].apply(LE.fit_transform)
+    
+    print(df.head)
 
     return df
+
+def outliers_statistics(df):
+
+    outliers_count = []
+
+    for col in df.columns:
+        q1 = df[col].quantile(0.25)
+        q3 = df[col].quantile(0.75)
+        IQR = q3 - q1
+        outliers = df[(df[col] < (q1 - 1.5 * IQR)) | (df[col] > (q3 + 1.5 * IQR))]
+        outliers_count.append(outliers.shape[0])
+
+    outliers_percentage = []
+    for value in outliers_count:
+        outliers_percentage.append(float(value)/len(df))
+
+    outliers_df = pd.DataFrame([outliers_count, outliers_percentage], columns=df.columns)
+    outliers_df.index = ['Ilość wartości skrajnych','Procent wartości skrajnych']
+
+    return outliers_df
+
+def remove_outliers(df2):
+    outliers_percentages=outliers_statistics(df2)
+    df=df2.copy()
+    for i, col in enumerate(df.columns):
+        q1 = df2[col].quantile(0.25)
+        q3 = df2[col].quantile(0.75)
+        IQR = q3 - q1
+        upper_bound=(q1 - 1.5 * IQR)
+        lower_bound=(q3 + 1.5 * IQR)
+        upper_limit = df2[col].mean() + 3*df2[col].std()
+        lower_limit = df2[col].mean() - 3*df2[col].std()
+        outliers_percentage = outliers_percentages.iloc[1,i]
+        if outliers_percentage < 0.05:
+            df[col] = df[col].where(~((df[col] < (q1 - 1.5 * IQR)) | ( df[col] > (q3 + 1.5 * IQR))))
+        else:
+            df[col] = df[col].clip(lower=lower_limit,upper=upper_limit)
+    cleaned_df=df.dropna()
+    
+    return cleaned_df
 
 
 if __name__ == "__main__":
     df = read_preprocessed_data()
     print(df["Age"].head())
+
+
