29f2ac79b31c7f51600aeef7322a4e4492abbcf2
Michał Korzeniewski <michal.korzeniewskiii@gmail.com>
2025-04-06 20:00:28 +0200

    fixed outlier functions not handling strings

--- notebooks/data.py
+++ notebooks/data.py
@@ -111,11 +111,14 @@ def outliers_statistics(df):
     outliers_count = []
 
     for col in df.columns:
-        q1 = df[col].quantile(0.25)
-        q3 = df[col].quantile(0.75)
-        IQR = q3 - q1
-        outliers = df[(df[col] < (q1 - 1.5 * IQR)) | (df[col] > (q3 + 1.5 * IQR))]
-        outliers_count.append(outliers.shape[0])
+        if df[col].dtypes in [float, int]:
+            q1 = df[col].quantile(0.25)
+            q3 = df[col].quantile(0.75)
+            IQR = q3 - q1
+            outliers = df[(df[col] < (q1 - 1.5 * IQR)) | (df[col] > (q3 + 1.5 * IQR))]
+            outliers_count.append(outliers.shape[0])
+        else:
+            outliers_count.append(0)
 
     outliers_percentage = []
     for value in outliers_count:
@@ -130,20 +133,21 @@ def remove_outliers(df2):
     outliers_percentages=outliers_statistics(df2)
     df=df2.copy()
     for i, col in enumerate(df.columns):
-        q1 = df2[col].quantile(0.25)
-        q3 = df2[col].quantile(0.75)
-        IQR = q3 - q1
-        upper_bound=(q1 - 1.5 * IQR)
-        lower_bound=(q3 + 1.5 * IQR)
-        upper_limit = df2[col].mean() + 3*df2[col].std()
-        lower_limit = df2[col].mean() - 3*df2[col].std()
-        outliers_percentage = outliers_percentages.iloc[1,i]
-        if outliers_percentage < 0.05:
-            df[col] = df[col].where(~((df[col] < (q1 - 1.5 * IQR)) | ( df[col] > (q3 + 1.5 * IQR))))
-        else:
-            df[col] = df[col].clip(lower=lower_limit,upper=upper_limit)
+        if df[col].dtypes in [float, int]:
+            q1 = df2[col].quantile(0.25)
+            q3 = df2[col].quantile(0.75)
+            IQR = q3 - q1
+            upper_bound=(q1 - 1.5 * IQR)
+            lower_bound=(q3 + 1.5 * IQR)
+            upper_limit = df2[col].mean() + 3*df2[col].std()
+            lower_limit = df2[col].mean() - 3*df2[col].std()
+            outliers_percentage = outliers_percentages.iloc[1,i]
+            if outliers_percentage < 0.05:
+                df[col] = df[col].where(~((df[col] < (q1 - 1.5 * IQR)) | ( df[col] > (q3 + 1.5 * IQR))))
+            else:
+                df[col] = df[col].clip(lower=lower_limit,upper=upper_limit)
     cleaned_df=df.dropna()
-    
+
     return cleaned_df
 
 
